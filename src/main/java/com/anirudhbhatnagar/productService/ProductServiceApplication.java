45474f8cfbf70e61e71de4a5373a2cc8b40128b5
Anirudh Bhatnagar <anirudhb@thoughtworks.com>
2019-11-03 20:55:40 +1100

    Added kubernetes deployment files

--- src/main/java/com/anirudhbhatnagar/productService/ProductServiceApplication.java
+++ src/main/java/com/anirudhbhatnagar/productService/ProductServiceApplication.java
@@ -1,9 +1,16 @@
 package com.anirudhbhatnagar.productService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableJpaRepositories
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
